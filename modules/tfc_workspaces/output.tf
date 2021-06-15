fdcf7f5a89232d8b53136129af1eeb5652d98836
scotttyso <tyscott@cisco.com>
2021-06-15 18:52:49 -0400

    provided more accurate output type

--- modules/tfc_workspaces/output.tf
+++ modules/tfc_workspaces/output.tf
@@ -1,7 +1,7 @@
 output "tfe_workspace_id" {
-  value = values(tfe_workspace.workspace)[*].id
+  value = { for k, v in tfe_workspace.workspace : k => tfe_workspace.workspace[v.working_directory].id }
 }
 
 output "tfe_workspace_name" {
-  value = values(tfe_workspace.workspace)[*].name
+  value = { for k, v in tfe_workspace.workspace : k => v.name }
 }
